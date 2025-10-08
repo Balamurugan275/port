30e2ae7f05aad72e537112a457f7ca63f459daf7
Your Name <you@example.com>
2025-10-08 10:47:14 +0530

    Standardize layout: base, navbar, home; clean views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,21 +1,22 @@
+
 from django.shortcuts import render
+from accounts.models import Profile
+
 
 def home(request):
     return render(request, 'portfolio/home.html')
 
-def about(request):
-    return render(request, 'portfolio/about.html')
-
-def projects(request):
-    return render(request, 'portfolio/projects.html')
-
-def contact(request):
-    return render(request, 'portfolio/contact.html')
-from django.shortcuts import render
-from accounts.models import Profile
 
 def about(request):
     profile = None
     if request.user.is_authenticated:
         profile = Profile.objects.filter(user=request.user).first()
-    return render(request, 'about.html', {'profile': profile})
+    return render(request, 'portfolio/about.html', {'profile': profile})
+
+
+def projects(request):
+    return render(request, 'portfolio/projects.html')
+
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
